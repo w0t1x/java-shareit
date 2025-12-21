77468c9a3cf371794cc341f0673b21015ef5135e
Rodion <oberemkoro@yandex.ru>
2025-12-22 01:35:27 +0300

    Наконец-то я видимо решил проблему с настройкой тестовой БД и работу сборки Maven...

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
+-- Удаление в правильном порядке (сначала дочерние таблицы)
+DELETE FROM comments;
+DELETE FROM bookings;
+DELETE FROM items;
+DELETE FROM users;
+
 -- Сброс счетчиков ID
 ALTER TABLE comments ALTER COLUMN ID RESTART WITH 1;
 ALTER TABLE bookings ALTER COLUMN ID RESTART WITH 1;
